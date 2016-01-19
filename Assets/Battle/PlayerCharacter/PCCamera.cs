b0399b6631a9559fa23247f0a2e2ba975a29f538
Seung Ho Jang <shjang1992@gmail.com>
2016-01-19 15:32:15 +0900

    PlayerCharacter camera class added

---
+++ Assets/Battle/PlayerCharacter/PCCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+class PCCamera : MonoBehaviour {
+    private void FixedUpdate()
+    {
+        var pc = GameObject.Find("Carp");
+        if (pc != null) {
+            transform.localPosition = new Vector3(pc.transform.localPosition.x + 5, pc.transform.localPosition.y, -10);
+        }
+    }
+}
